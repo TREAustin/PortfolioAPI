e7bc63cb816be0cf85bb25b5ad624b20b1362d23
TREAustin <tausti0065@gmail.com>
2023-01-28 10:27:06 -0600

    Removed Google credentials.  Will be moving it to a secrets file later.

--- PortfolioAPI/Controllers/ContactController.cs
+++ PortfolioAPI/Controllers/ContactController.cs
@@ -17,44 +17,44 @@ namespace PortfolioAPI.Controllers
             _context = context;
         }
 
-        [HttpPost]
-        [Route("IsContacting")]
-        public bool IsContacting(Contact contact)
-        {
-            if (contact == null)
-            {
-                return false;
-            }
+        //[HttpPost]
+        //[Route("IsContacting")]
+        //public bool IsContacting(Contact contact)
+        //{
+        //    if (contact == null)
+        //    {
+        //        return false;
+        //    }
 
-            try
-            {
-                MailMessage msg = new()
-                {
-                    From = new MailAddress(contact.Email)
-                };
-                msg.Subject = $"{contact.Name} is reaching out.";
-                msg.IsBodyHtml = true;
-                msg.To.Add(new MailAddress("tausti0065@gmail.com"));
-                msg.Body = "<h2>Name: " + contact.Name + "</h2><h2>Email: " + contact.Email + "</h2><h2>" + contact.Reason + "</h2>";
-                msg.Sender = new MailAddress(contact.Email);
+        //    try
+        //    {
+        //        MailMessage msg = new()
+        //        {
+        //            From = new MailAddress(contact.Email)
+        //        };
+        //        msg.Subject = $"{contact.Name} is reaching out.";
+        //        msg.IsBodyHtml = true;
+        //        msg.To.Add(new MailAddress("tausti0065@gmail.com"));
+        //        msg.Body = "<h2>Name: " + contact.Name + "</h2><h2>Email: " + contact.Email + "</h2><h2>" + contact.Reason + "</h2>";
+        //        msg.Sender = new MailAddress(contact.Email);
 
-                SmtpClient smtpClient = new SmtpClient
-                {
-                    Host = "smtp.gmail.com",
-                    Port = 587,
-                    DeliveryMethod = SmtpDeliveryMethod.Network,
-                    UseDefaultCredentials = false,
-                    EnableSsl = true,
-                    Credentials = new NetworkCredential("tausti0065@gmail.com", "lbvtmfrfmzveihks")
-                };
-                smtpClient.Send(msg);
-            }
-            catch (Exception e)
-            {
-                System.Diagnostics.Debug.WriteLine(e);
-            }
+        //        SmtpClient smtpClient = new SmtpClient
+        //        {
+        //            Host = "smtp.gmail.com",
+        //            Port = 587,
+        //            DeliveryMethod = SmtpDeliveryMethod.Network,
+        //            UseDefaultCredentials = false,
+        //            EnableSsl = true,
+        //            Credentials = new NetworkCredential("tausti0065@gmail.com", "")
+        //        };
+        //        smtpClient.Send(msg);
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        System.Diagnostics.Debug.WriteLine(e);
+        //    }
 
-            return true;
-        }
+        //    return true;
+        //}
     }
 }
