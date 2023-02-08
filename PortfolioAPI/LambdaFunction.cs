7f306c24d44054e8acf2bfb3f033748cba40a6d0
TREAustin <tausti0065@gmail.com>
2023-02-08 10:52:22 -0600

    Removed C# backend, added lambda function in AWS for python to retrieve and send data for examples to front end.

---
+++ PortfolioAPI/LambdaFunction.cs
@@ -0,0 +1,13 @@
+﻿namespace PortfolioAPI
+{
+    public class LambdaFunction : Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
+    {
+        protected override void Init(IWebHostBuilder builder)
+        {
+            builder
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup(Directory.GetCurrentDirectory())
+                .UseApiGateway();
+        }
+    }
+}
